3a75b1c8ab553ca278976294709f85d57e7e3c4f
Dark Night <darknight.dev0918@gmail.com>
2023-02-18 02:04:38 +0900

    Changed Mobile Responsive

--- src/pages/dashboard/section3.jsx
+++ src/pages/dashboard/section3.jsx
@@ -5,8 +5,8 @@ import NFTTwo from '../../assets/NFT_2.png'
 
 const Section3 = () => {
   return (
-    <div className='min-[980px]:flex mt-[50px] px-[5vw] gap-5'>
-        <div className='mt-[80px] md:mt-[60px] text-[16px] md:text-[18px] xl:text-[24px] text-center text-white min-[980px]:text-left'>
+    <div className='min-[980px]:flex mt-[50px] px-[3vw] sm:px-[5vw] gap-5'>
+        <div className='mt-[80px] md:mt-[60px] text-[14px] md:text-[18px] xl:text-[24px] text-center text-white min-[980px]:text-left'>
             <h1 className='text-[#3A9069] font-extrabold text-[40px] md:text-[50px] xl:text-[72px] text-transparent bg-clip-text bg-gradient-to-r from-[#3a906950] to-[#3A9069]'>Why NFTs?</h1>
             <p>NFTs, or non-fungible tokens, offer a</p>
             <p>revolutionary way to represent digital assets as</p>
