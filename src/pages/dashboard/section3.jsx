6551131cd183cd8b34a49941a2231740bf79ff30
Dark Night <darknight.dev0918@gmail.com>
2023-02-18 02:18:20 +0900

    Why NFT text line Upgraded

--- src/pages/dashboard/section3.jsx
+++ src/pages/dashboard/section3.jsx
@@ -5,7 +5,7 @@ import NFTTwo from '../../assets/NFT_2.png'
 
 const Section3 = () => {
   return (
-    <div className='min-[980px]:flex mt-[50px] px-[3vw] sm:px-[5vw] gap-5'>
+    <div className='min-[980px]:flex mt-[50px] px-[3vw] sm:px-[5vw] gap-5 justify-between'>
         <div className='mt-[80px] md:mt-[60px] text-[14px] md:text-[18px] xl:text-[24px] text-center text-white min-[980px]:text-left'>
             <h1 className='text-[#3A9069] font-extrabold text-[40px] md:text-[50px] xl:text-[72px] text-transparent bg-clip-text bg-gradient-to-r from-[#3a906950] to-[#3A9069]'>Why NFTs?</h1>
             <p>NFTs, or non-fungible tokens, offer a</p>
@@ -14,7 +14,7 @@ const Section3 = () => {
             <p>opportunities for creators, collectors, and</p>
             <p>investors alike</p>
         </div>
-        <div className='w-full mt-10 min-[980px]:mt-0 min-[980px]:w-[640px] flex gap-2 justify-center min-[980px]:justify-end'>
+        <div className='mt-10 w-full min-[980px]:w-[48%] min-[980px]:mt-0 flex gap-2 justify-center min-[980px]:justify-end'>
             <div>
                 <img src = {NFTOne} alt= "NFTOne" className='mt-[90px]'/>
             </div>
